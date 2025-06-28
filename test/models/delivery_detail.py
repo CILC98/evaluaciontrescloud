985c8133457f0a72a384ff7e820ac0fb5510c11c
Santiago Villegas <guevara-villegas@hotmail.com>
2025-06-28 16:12:58 -0500

    [IMP][CL] Codigo Cristopher

--- test/models/delivery_detail.py
+++ test/models/delivery_detail.py
@@ -7,16 +7,50 @@ from odoo import models, fields, api
 class DeliveryDetail(models.Model):
     _name = 'delivery.detail'
 
+    # ------------------------------------------------------
+    # ACTIONS
+    # ------------------------------------------------------
+
+    # ------------------------------------------------------
+    # CRUD METHODS
+    # ------------------------------------------------------
+    # @api.model_create_multi
+
+    # ------------------------------------------------------
+    # COMPUTE METHODS
+    # ------------------------------------------------------
+
+    # ------------------------------------------------------
+    # CONSTRAINTS AND VALIDATIONS
+    # ------------------------------------------------------
+
+    # ------------------------------------------------------
+    # ONCHANGE METHODS
+    # ------------------------------------------------------
+
+    # ------------------------------------------------------
+    # OTHER METHODS
+    # ------------------------------------------------------
+
+    # ------------------------------------------------------
+    # VARIABLES
+    # ------------------------------------------------------
+
     account_move_id = fields.Many2one(
         'account.move',
+        string='Factura',
+        help="Factura asociada a este detalle de entrega."
     )
     picking_id = fields.Many2one(
         'stock.picking',
+        string='Transferencia',
+        help="Transferencia asociada a este detalle de entrega."
     )
-
     invoiced = fields.Boolean(
+        string='Facturado',
+        default=False,
+        help="Indica si el detalle de la orden ha sido facturado por completo."
     )
-
     delivery_detail_line_ids = fields.One2many(
         'delivery.detail.line',
         'delivery_detail_id',
